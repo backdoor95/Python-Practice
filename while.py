78426447735899575a250ad857e0578453b9a6e1
junhoMun <jhmun0607@gmail.com>
2024-04-19 16:59:16 +0900

    feat: while02

--- while.py
+++ while.py
@@ -18,4 +18,45 @@ while number != 4:
     print(prompt)
     number = int(input()) #사용자의 숫자 입력을 받아들이는 것.
 
-# while문 강제로 빠져나가기
+# while문 강제로 빠져나가기 break 
+'''
+
+# coffee.py
+coffee = 10
+while True:
+    money = int(input("돈을 넣어 주세요: "))
+    if money == 300:
+        print("커피를 줍니다.")
+        coffee = coffee -1
+    elif money > 300:
+        print("거스름돈 %d를 주고 커피를 줍니다." % (money -300))
+        coffee = coffee -1
+    else:
+        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+        print("남은 커피의 양은 %d개 입니다." % coffee)
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다. 판매를 중지 합니다.")
+        break
+
+
+'''
+
+
+# while문의 맨 처음으로 돌아가기
+'''
+
+>>> a = 0
+>>> while a < 10:
+...     a = a + 1
+...     if a % 2 == 0: continue
+...     print(a)
+...
+1
+3
+5
+7
+9
+
+
+'''
+
