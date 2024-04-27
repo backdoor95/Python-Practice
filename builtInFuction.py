3bf096a762e2150765b6f67a3404d5384e24a3a0
MunJunHo <mjh1995713@gmail.com>
2024-04-27 21:47:32 +0900

    feat: 내장함수

---
+++ builtInFuction.py
@@ -0,0 +1,11 @@
+# int 소수점 아래는 버리고 정수만 출력
+print(int(4.7))
+
+a =[3,1,2]
+
+print(sorted(a))
+
+print(list(zip("abc", "def" , "z")))
+print(list(zip("abc", "def" , "zz")))
+print(list(zip("abc", "def" , "zzz")))
+print(list(zip("abc", "def" , "zzzz")))
